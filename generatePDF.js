78f00d486d7f303f130c5a7838f0ac264d179a21
Amarjeet <amarjeetofficial81@gmail.com>
2024-08-18 11:31:00 +0530

    fixing certificate points

--- generatePDF.js
+++ generatePDF.js
@@ -12,7 +12,9 @@ const doc = new jsPDF({
     unit: 'px'
 })
 
-//fonts
+const margin = 15
+
+//FONTS
 //font for headline
 const fontForHeadline = fs.readFileSync(path.join(__dirname, 'fonts', 'EduVICWANTBeginner-VariableFont_wght.ttf'))
 //base64 font for headline
@@ -21,7 +23,7 @@ const base64FontForHeadline = fontForHeadline.toString('base64')
 doc.addFileToVFS("EduVICWANTBeginner-VariableFont_wght.ttf", base64FontForHeadline);
 doc.addFont("EduVICWANTBeginner-VariableFont_wght.ttf", "Edu", "normal", "bold");
 
-const margin = 15
+
 
 //finding width and hight of the page
 const pageHeight = doc.internal.pageSize.getHeight()
@@ -30,12 +32,31 @@ const pageWidth = doc.internal.pageSize.getWidth()
 //fetures for headline
 const username = 'Amarjeet'
 const headline = `I, ${username}, hereby solemnly pledge to uphold and embody the following principles, which reflect the true spirit of being an Indian:`
+
 //headline is too long soo increse the line count
 //headline width
 const headlineWidth = doc.getTextWidth(headline)
 //headline center
 const headlineWidthCenter = headlineWidth / 2
 
+//fetures for points
+const points = ['I love my India and uphold its dignity at all times.', 'I respect women and am committed to ensuring their safety and dignity.', 'I respect and embrace diversity in religion, gender, caste, and region.', 'I uphold and respect the principles of the Indian Constitution.', 'I actively engage in questioning the government and value political literacy.', 'I take responsibility for the well-being of society and contribute to social upliftment.', 'I prioritize environmental stewardship for a sustainable future.', 'I focus on continuous education and personal growth.']
+
+
+//maping points
+function point(){
+    points.forEach((e, index) => {
+    return e
+})
+}
+
+//font for points
+const fontForPoints = fs.readFileSync(path.join(__dirname, 'fonts', 'Caveat-VariableFont_wght.ttf'))
+//base64 font for points
+const base64FontForPoints = fontForPoints.toString('base64')
+
+doc.addFileToVFS("Caveat-VariableFont_wght.ttf", base64FontForPoints);
+doc.addFont("Caveat-VariableFont_wght.ttf", "Caveat", "normal", "bold");
 
 //finding center of the page
 const verticalCenter = pageHeight / 2
@@ -55,13 +76,38 @@ const coordinateXforimage = horizontalCenter - (imageWidth / 2)
 const coordinateYforimage = verticalCenter - (imageHeight / 2)
 
 
-function generatePDF(name){
+function generatePDF(name) {
+    //adding reactangle
     doc.rect(margin, margin, pageWidth - margin * 2, pageHeight - margin * 2);
+    //adding image
+    doc.addImage(base64Image, 'PNG', coordinateXforimage, coordinateYforimage, imageWidth, imageHeight,)
+    //adding headline
     doc.setFontSize(20)
     doc.setFont("Edu", "bold")
     const multilineHeadline = doc.splitTextToSize(headline, pageWidth - margin * 3.5)
-    doc.text(multilineHeadline, margin * 2, margin * 3, doc.fint)
-    doc.addImage(base64Image, 'PNG', coordinateXforimage, coordinateYforimage, imageWidth,imageHeight, )
+    doc.text(multilineHeadline, margin * 2, margin * 3)
+    //adding points
+    doc.setFontSize(20)
+    doc.setFont("Caveat", "bold")
+    let pushPoint = 0
+    points.forEach((e, index)=>{
+    const point = `${index + 1} ${e}`
+    const multilinePoint = doc.splitTextToSize(point, pageWidth - margin * 6)
+    const linecount = multilinePoint.length
+    // if(linecount < pushPoint){
+    //     pushPoint = linecount
+    // }
+    doc.text(multilinePoint, margin * 3, margin * (9+index+2) + pushPoint*6)
+    if(linecount != 1){
+        // pushPoint = 0
+        pushPoint = linecount
+    }
+    // else if (linecount < pushPoint) {
+    //     pushPoint = 0
+    // }
+    console.log(linecount, pushPoint)
+    })
+
     doc.save('example.pdf')
 }
 
